6b7ce50552f2bdae69df84d285839a677f4d732f
Vadim Rutkovsky <vadim@vrutkovs.eu>
2026-03-20 16:51:30 +0100

    load_test: check k6 metrics

--- tests/load_test/load_test.go
+++ tests/load_test/load_test.go
@@ -85,17 +85,18 @@ var _ = Describe("Load tests", Ordered, ContinueOnFailure, Label("load-test"), f
 		It("Default installation should handle prometheus remote write v2 insert and read", Label("id=a1b2c3d4-e5f6-7890-abcd-ef1234567890"), func() {
 			By("Run 50vus-10mins scenario")
 			scenario := "prw2-50vus-10mins"
+			parallelism := 3
 
-			err := install.RunK6Scenario(ctx, t, consts.K6TestsNamespace, consts.DefaultVMNamespace, scenario, 3)
+			err := install.RunK6Scenario(ctx, t, consts.K6TestsNamespace, consts.DefaultVMNamespace, scenario, parallelism)
 			require.NoError(t, err)
 
 			By("Waiting for K6 jobs to complete")
-			install.WaitForK6JobsToComplete(ctx, t, consts.K6TestsNamespace, scenario, 3)
+			install.WaitForK6JobsToComplete(ctx, t, consts.K6TestsNamespace, scenario, parallelism)
 
 			By("PRW v2 rows were inserted without errors")
 			_, value, err := overwatch.VectorScan(ctx, "sum(vm_rows_inserted_total)")
 			require.NoError(t, err)
-			require.Greater(t, value, float64(0))
+			require.Greater(t, value, float64(200_000))
 
 			By("No rows were ignored")
 			_, value, err = overwatch.VectorScan(ctx, "sum(vm_rows_ignored_total)")
@@ -105,6 +106,32 @@ var _ = Describe("Load tests", Ordered, ContinueOnFailure, Label("load-test"), f
 			_, value, err = overwatch.VectorScan(ctx, "sum(vm_rows_invalid_total)")
 			require.NoError(t, err)
 			require.Equal(t, value, model.SampleValue(0))
+
+			By("k6 insert and read requests were made")
+			_, value, err = overwatch.VectorScan(ctx, `sum(k6_http_reqs_total{scenario="insert"})`)
+			require.NoError(t, err)
+			require.Greater(t, value, float64(1_300_000))
+
+			_, value, err = overwatch.VectorScan(ctx, `sum(k6_http_reqs_total{scenario="read"})`)
+			require.NoError(t, err)
+			require.Greater(t, value, float64(10_000))
+
+			By("No k6 requests failed")
+			_, value, err = overwatch.VectorScan(ctx, `sum(k6_http_req_failed_rate{scenario="insert"})`)
+			require.NoError(t, err)
+			require.Equal(t, value, model.SampleValue(0))
+
+			_, value, err = overwatch.VectorScan(ctx, `sum(k6_http_req_failed_rate{scenario="read"})`)
+			require.NoError(t, err)
+			require.Equal(t, value, model.SampleValue(0))
+
+			_, value, err = overwatch.VectorScan(ctx, `sum(k6_http_req_duration_p95{scenario="insert"})`)
+			require.NoError(t, err)
+			require.Less(t, value, float64(0.5))
+
+			_, value, err = overwatch.VectorScan(ctx, `sum(k6_http_req_duration_p95{scenario="read"})`)
+			require.NoError(t, err)
+			require.Less(t, value, float64(10))
 		})
 
 		PIt("Default installation should handle 50vus-30mins load test scenario", Label("kind", "id=d37b1987-a9e7-4d13-87b7-f2ded679c249"), func() {
