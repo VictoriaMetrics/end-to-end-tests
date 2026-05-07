4da0f607e9adbc7391bf19fc5de8ef61eb806501
Vadim Rutkovsky <vadim@vrutkovs.eu>
2026-05-07 18:51:47 +0200

    fix: update test dir mounts

--- .buildkite/generate_pipeline.py
+++ .buildkite/generate_pipeline.py
@@ -100,7 +100,7 @@ def make_step(
     procs: int,
     flakes: int,
 ) -> dict:
-    make_cmd = f"make test-gke TEST_BINARY=/tests/{suite}_test.test PROCS={procs} FLAKE_ATTEMPTS={flakes} TIMEOUT=90m BUILD_ID={build_number} REPORT_DIR=/tests/allure-results"
+    make_cmd = f"make test-gke TEST_BINARY=/tests/{suite}_test.test PROCS={procs} FLAKE_ATTEMPTS={flakes} TIMEOUT=90m BUILD_ID={build_number} REPORT_DIR=./allure-results"
     if is_enterprise or is_lts_current or is_lts_previous:
         make_cmd += " LICENSE_FILE=/buildkite-secrets/license.txt VM_ENTERPRISE=1"
     if is_rc:
@@ -114,7 +114,7 @@ def make_step(
     if branch == "main":
         upload_results = textwrap.dedent(f"""\
             echo "--- Uploading results"
-            make upload-results TEST_SUITE={suite} BUILD_ID={build_number} REPORT_DIR=/tests/allure-results
+            make upload-results TEST_SUITE={suite} BUILD_ID={build_number} REPORT_DIR=./allure-results
             """)
 
     command = textwrap.dedent(
@@ -141,7 +141,6 @@ def make_step(
                     "volumes": [
                         "/tmp:/tmp",
                         "/buildkite-secrets:/buildkite-secrets",
-                        "$BUILDKITE_BUILD_CHECKOUT_PATH/allure-results:/tests/allure-results",
                     ],
                 }
             }
