36207f0c61c27fff0ebb983e90fca4496c13b5d6
Vadim Rutkovsky <vadim@vrutkovs.eu>
2026-05-19 11:20:38 +0200

    feat: use ramping arrival rate for KEDA

--- manifests/load-tests/prw2-50vus-10mins.js
+++ manifests/load-tests/prw2-50vus-10mins.js
@@ -2,27 +2,31 @@ import http from "k6/http";
 import { check } from "k6";
 import { randomIntBetween } from "https://jslib.k6.io/k6-utils/1.2.0/index.js";
 
-const K6_DURATION = __ENV.SCENARIO_DURATION || "10m";
-
 export const options = {
   scenarios: {
     insert: {
-      executor: "constant-arrival-rate",
-      duration: K6_DURATION,
-      rate: 150,
+      executor: "ramping-arrival-rate",
+      startRate: 0,
       timeUnit: "1s",
       preAllocatedVUs: 50,
       maxVUs: 500,
       exec: "insert",
+      stages: [
+        { duration: "5m", target: 150 },
+        { duration: "5m", target: 0 },
+      ],
     },
     read: {
-      executor: "constant-arrival-rate",
-      duration: K6_DURATION,
-      rate: 40,
+      executor: "ramping-arrival-rate",
+      startRate: 0,
       timeUnit: "1s",
       preAllocatedVUs: 50,
       maxVUs: 500,
       exec: "read",
+      stages: [
+        { duration: "5m", target: 40 },
+        { duration: "5m", target: 0 },
+      ],
     },
   },
   insecureSkipTLSVerify: true,
