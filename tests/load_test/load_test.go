6034a543f9e3d5d5de511fa1afdb50d6490ce78f
Vadim Rutkovsky <vadim@vrutkovs.eu>
2026-06-03 15:48:59 +0200

    fix: tweak nfs read baseline

--- tests/load_test/load_test.go
+++ tests/load_test/load_test.go
@@ -556,7 +556,7 @@ var _ = Describe("Load tests", Label("load-test"), func() {
 				checkMetric(
 					"k6 read requests were made",
 					fmt.Sprintf(`max_over_time(sum(k6_http_reqs_total{scenario="read", job_name=~"%s.*"})[15m])`, scenarioName),
-				).Greater(15_000)
+				).Greater(14_000)
 
 				checkMetric(
 					"k6 insert requests failure rate is acceptable",
