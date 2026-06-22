d2157315479de8166996446df61ecc2486dd9102
Vadim Rutkovsky <vadim@vrutkovs.eu>
2026-06-22 14:06:26 +0200

    test: decrease expected metric threshold in load test

--- tests/load_test/load_test.go
+++ tests/load_test/load_test.go
@@ -773,7 +773,7 @@ var _ = Describe("Load tests", Label("load-test"), func() {
 				checkMetric(
 					"VMAgent forwarded rows to VMInsert",
 					fmt.Sprintf(`max_over_time(sum(vmagent_remotewrite_rows_pushed_after_relabel_total{namespace="%s"})[15m])`, namespace),
-				).Greater(4_000_000)
+				).Greater(2_500_000)
 			},
 		}),
 	)
