04b2ce4a0c49f0e597bd6a5cdc7b70aa85598bbc
Vadim Rutkovsky <vadim@vrutkovs.eu>
2026-07-31 14:59:44 +0200

    feat: vmgather should not compress traffic from VM

--- pkg/exporter/types.go
+++ pkg/exporter/types.go
@@ -9,13 +9,14 @@ type Auth struct {
 
 // Connection defines the connection details for the VictoriaMetrics instance.
 type Connection struct {
-	URL           string `json:"url"`
-	APIBasePath   string `json:"api_base_path"`
-	TenantID      *int   `json:"tenant_id"` // Use *int for nullability
-	IsMultitenant bool   `json:"is_multitenant"`
-	FullAPIURL    string `json:"full_api_url"`
-	Auth          Auth   `json:"auth"`
-	SkipTLSVerify bool   `json:"skip_tls_verify"`
+	URL                string `json:"url"`
+	APIBasePath        string `json:"api_base_path"`
+	TenantID           *int   `json:"tenant_id"` // Use *int for nullability
+	IsMultitenant      bool   `json:"is_multitenant"`
+	FullAPIURL         string `json:"full_api_url"`
+	Auth               Auth   `json:"auth"`
+	SkipTLSVerify      bool   `json:"skip_tls_verify"`
+	DisableCompression bool   `json:"disable_compression,omitempty"`
 }
 
 // TimeRange defines the start and end times for data export.
