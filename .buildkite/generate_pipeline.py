80ec862d6727ac7179b7cf75e3035bd11debb61b
Vadim Rutkovsky <vadim@vrutkovs.eu>
2026-08-21 07:29:10 +0200

    ci: Disable Kubernetes 1.28 and 1.29 tests

--- .buildkite/generate_pipeline.py
+++ .buildkite/generate_pipeline.py
@@ -75,8 +75,10 @@ K8S_VERSIONS = [
     "1.32",
     "1.31",
     "1.30",
-    #"1.29", disabled until preStop Sleep handler is handled by operator (0.74+)
-    "1.28",
+
+    # disabled until preStop Sleep handler is handled by operator (0.74+)
+    #"1.29",
+    #"1.28",
 ]
 
 SUITES = [
