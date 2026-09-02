4b31d2455ee20602a4fa601b2f448770b2a4b37e
Vadim Rutkovsky <vadim@vrutkovs.eu>
2026-09-02 15:47:52 +0200

    fix: wait for NFS device before limiting I/O

--- pkg/install/scripts/nfs-io-limiter.sh
+++ pkg/install/scripts/nfs-io-limiter.sh
@@ -1,12 +1,17 @@
-set -eu
+set -eux
 
 cgroup=$(awk -F: '$1 == "0" {print $3}' /proc/1/cgroup)
 test -n "$cgroup"
 cgroup_dir="/sys/fs/cgroup$cgroup"
 test -f "$cgroup_dir/io.max"
 
-device=$(awk '$1 ~ /^[0-9]+:[0-9]+$/ && ($2 ~ /rbytes=[1-9]/ || $3 ~ /wbytes=[1-9]/) {print $1; exit}' "$cgroup_dir/io.stat")
-test -n "$device"
+# Device may be absent until NFS clients issue first I/O during pod startup.
+while :; do
+  device=$(awk '$1 ~ /^[0-9]+:[0-9]+$/ && ($2 ~ /rbytes=[1-9]/ || $3 ~ /wbytes=[1-9]/) {print $1; exit}' "$cgroup_dir/io.stat")
+  test -n "$device" && break
+  sleep 1
+done
+
 echo "$device rbps=$NFS_IO_BYTES_PER_SECOND wbps=$NFS_IO_BYTES_PER_SECOND riops=$NFS_IOPS wiops=$NFS_IOPS" > "$cgroup_dir/io.max"
 cat "$cgroup_dir/io.max"
 sleep infinity
